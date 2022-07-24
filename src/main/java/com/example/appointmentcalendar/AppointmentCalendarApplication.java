a3b884d3b13112bd24eaa81b6ffd942cfdf90121
bashar <basharkhan6@gmail.com>
2022-07-24 17:14:02 +0600

    Initialized Backend and UI

---
+++ src/main/java/com/example/appointmentcalendar/AppointmentCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.example.appointmentcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppointmentCalendarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/appointmentcalendar/AppointmentCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appointmentcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentCalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
